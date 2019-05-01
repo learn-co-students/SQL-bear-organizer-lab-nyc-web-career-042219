ea60193d5114d40bbcac287a03f1b8850433e662
henryk1229 <henryk1229@gmail.com>
2019-04-30 23:33:47 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY, 
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+
+
+
+
+-- CREATE TABLE cats (
+--   name TEXT,
+--   age INTEGER,
+--   breed TEXT,
+--   net_worth INTEGER
+-- );
